8c8a06bc1066e1ff7bf94e38fe0723fcda46431c
Santiillana <131924179+Santiillana@users.noreply.github.com>
2026-09-24 09:30:42 +0000

    fix(s): corregir sintaxis final de migración v14

--- src/db/database.ts
+++ src/db/database.ts
@@ -991,7 +991,7 @@ class Database {
     const add = async (table: string, column: string) => {
       const cols = await this.columnasDeTabla(table);
       if (!cols.has(column)) {
-        await this.conn().execute('ALTER TABLE ' + table + ' ADD COLUMN ' + column + " TEXT NOT NULL DEFAULT '';";, false);
+        await this.conn().execute(`ALTER TABLE ${table} ADD COLUMN ${column} TEXT NOT NULL DEFAULT '';`, false);
       }
     };
     await add('clientes','nombre_normalizado');
